365ff21bd94b0c7b461b7a29903bbe57504e6683
jabber <2898117012@qq.com>
2022-02-17 21:51:30 +0800

    fix: event

--- src/Events/Event.php
+++ src/Events/Event.php
@@ -3,9 +3,14 @@
 namespace Dealskoo\Favorite\Events;
 
 use Dealskoo\Favorite\Models\Favorite;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class Event
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
     public $favorite;
 
     public function __construct(Favorite $favorite)
--- src/Events/Unfavorited.php
+++ src/Events/Unfavorited.php
@@ -2,6 +2,11 @@
 
 namespace Dealskoo\Favorite\Events;
 
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 class Unfavorited extends Event
 {
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 }
